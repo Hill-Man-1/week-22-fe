27c2f8a531da50c8bcb2b0029d038276de751640
Hilman Syarifudin F <38031999+Hill-Man-1@users.noreply.github.com>
2023-12-14 07:32:06 +0700

    Delete src directory

--- src/components/PrivateRouter.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-
-const PrivateRoute = ({ children }) => {
-    const { user } = useSelector((state) => state.user);
-    return user ? children : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
